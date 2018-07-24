f18a5297d41f7464c18bf288bab7a6993dd089c2
Colin Jean-saunier <js_colin@hotmail.fr>
2018-07-24 19:50:26 +0200

    next commit

--- joinMeApi/settings.py
+++ joinMeApi/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -39,7 +40,7 @@ if os.environ.get('ENV') == 'PRODUCTION':
 else:
     DEBUG = True
 
-ALLOWED_HOSTS = ['joinmeapi.herokuapp.com']
+ALLOWED_HOSTS = ['joinmeapi-dev.herokuapp.com']
 
 
 # Application definition
@@ -131,6 +132,9 @@ DATABASES = {
     }
 }
 
+if os.environ.get('ENV') == 'HEROKU':
+    db_from_env = dj_database_url.config(conn_max_age=500)
+    DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
