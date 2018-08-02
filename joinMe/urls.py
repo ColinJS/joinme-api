aebaf4fc567f6722c56783067402be2c969b3ef1
Colin Jean-saunier <js_colin@hotmail.fr>
2018-08-02 16:26:19 +0200

    server upload debug

--- joinMe/urls.py
+++ joinMe/urls.py
@@ -2,9 +2,9 @@ from django.conf.urls import url
 from joinMe import views
 
 urlpatterns = [
-    url(r'^init', views.FirstConnection.as_view()),
-    url(r'^events', views.EventList.as_view()),
-    url(r'^friends', views.FriendList.as_view()),
-    url(r'^(?P<event_id>[0-9]+)\/share', views.SharingEvent.as_view()),
+    url(r'^init$', views.FirstConnection.as_view()),
+    url(r'^events$', views.EventList.as_view()),
+    url(r'^friends$', views.FriendList.as_view()),
+    url(r'^(?P<event_id>[0-9]+)\/share$', views.SharingEvent.as_view()),
     url(r'^events/(?P<event_id>[0-9]+)$', views.eventDetails.as_view()),
 ]
