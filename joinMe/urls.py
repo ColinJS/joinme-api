639d34a6ed5555b29f3533a14a34326a9cec556d
Colin Jean-saunier <js_colin@hotmail.fr>
2018-08-29 18:48:22 +0200

    in app notif

--- joinMe/urls.py
+++ joinMe/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     url(r'^(?P<event_id>[0-9]+)\/share$', views.SharingEvent.as_view()),
     url(r'^events/(?P<event_id>[0-9]+)$', views.EventDetails.as_view()),
     url(r'^sign_s3$', views.aws_s3_interface.as_view()),
+    url(r'^notification$', views.Notifications.as_view()),
 ]
