564cf6ef1f9419cc046953c25d0cf7206a75c793
ColinJS <js_colin@hotmail.fr>
2018-10-12 17:02:34 +0200

    closse connections

--- joinMe/routing.py
+++ joinMe/routing.py
@@ -4,5 +4,5 @@ from django.conf.urls import url
 from . import consumers
 
 websocket_urlpatterns = [
-    url(r'^ws/event/(?P<event_id>[^/]+)/$', consumers.EventConsumer),
+    url(r'^ws/event/(?P<event_id>[0-9]+)/$', consumers.EventConsumer),
 ]
