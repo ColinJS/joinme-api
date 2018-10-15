f96686e60ef4582c6ff6c20046de66e713f45b95
ColinJS <js_colin@hotmail.fr>
2018-10-15 15:50:07 +0200

    notification

--- joinMe/routing.py
+++ joinMe/routing.py
@@ -5,5 +5,5 @@ from . import consumers
 
 websocket_urlpatterns = [
     url(r'^ws/event/(?P<event_id>[0-9]+)/$', consumers.EventConsumer),
-    url(r'^ws/user/(?P<user_id>[0-9]+)/$', consumers.EventConsumer),
+    url(r'^ws/user/(?P<user_id>[0-9]+)/$', consumers.UserConsumer),
 ]
