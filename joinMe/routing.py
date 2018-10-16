35952efd6d55254fdd74aeadf901021da0ec7680
ColinJS <js_colin@hotmail.fr>
2018-10-16 16:22:28 +0200

    websocket: custom authentication

--- joinMe/routing.py
+++ joinMe/routing.py
@@ -5,5 +5,5 @@ from . import consumers
 
 websocket_urlpatterns = [
     url(r'^ws/(?P<token>[0-9A-Za-z]+)/event/(?P<event_id>[0-9]+)/$', consumers.EventConsumer),
-    url(r'^ws/(?P<token>[0-9A-Za-z]+)/user/(?P<user_id>[0-9]+)/$', consumers.UserConsumer),
+    url(r'^ws/(?P<token>[0-9A-Za-z]+)/user/$', consumers.UserConsumer),
 ]
