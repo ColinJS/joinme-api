d7cd1174fd06f3f4738d587d71fbb7fb1c814376
ColinJS <js_colin@hotmail.fr>
2018-10-22 18:35:14 +0200

    admin interface

--- joinMe/admin.py
+++ joinMe/admin.py
@@ -1,15 +1,22 @@
 from django.contrib import admin
 from joinMe.models import Profile, Avatar, Event, GuestToEvent, Notification, Video, Place
+from django.utils.safestring import mark_safe
 
 class GuestsInline(admin.TabularInline):
     model = GuestToEvent
     fieldsets = [
         (None, {'fields': ['guest', 'state']})
     ]
+    ordering = ('state',)
 
 @admin.register(Event)
 class EventAdmin(admin.ModelAdmin):
+    fields = ['created_by', 'duration', 'created', 'video_url']
     inlines = [GuestsInline, ]
+
+    def video_url(self, event):
+        url = event.videos.video
+        return mark_safe("<a href='{}'>{}</a>".format(url, url))
 
 
 admin.site.register(Video)
