e8f750c51b9decbb5eef8feb68c156da86a447dc
ColinJS <js_colin@hotmail.fr>
2018-10-22 17:15:55 +0200

    admin interface

--- joinMe/admin.py
+++ joinMe/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
 from joinMe.models import Profile, Avatar, Event, GuestToEvent, Notification
 
-admin.site.register(Event)
+
+class EventAdmin(admin.ModelAdmin):
+    fieldsets = (
+        (None, {
+            'fields': ('created_by', 'duration', 'videos', 'guests', 'place')
+        })
+    )
+
+
+admin.site.register(Event, EventAdmin)
+
+
 
