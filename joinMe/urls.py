31e88d11bcd941ba2bcd4d3def67cbe16a51acd6
ColinJS <js_colin@hotmail.fr>
2018-10-23 16:49:24 +0200

    add groups friends

--- joinMe/urls.py
+++ joinMe/urls.py
@@ -1,12 +1,15 @@
 from django.conf.urls import url
 from joinMe import views
+from rest_framework import routers
+
+url_router = routers.SimpleRouter()
+url_router.register(r'groups', views.UserGroupEndPoint)
 
 urlpatterns = [
     url(r'^init$', views.FirstConnection.as_view()),
     url(r'^users$', views.Users.as_view()),
     url(r'^events$', views.EventList.as_view()),
     url(r'^friends$', views.FriendList.as_view()),
-    url(r'^groups$', views.UserGroupEndPoint.as_view()),
     url(r'^friends/(?P<user_id>[0-9]+)\/state', views.FriendshipManager.as_view()),
     url(r'^(?P<event_id>[0-9]+)\/share$', views.SharingEvent.as_view()),
     url(r'^events/(?P<event_id>[0-9]+)$', views.EventDetails.as_view()),
@@ -15,3 +18,4 @@ urlpatterns = [
     url(r'^notifications$', views.Notifications.as_view()),
     url(r'^me$', views.Me.as_view()),
 ]
+urlpatterns += url_router.urls
